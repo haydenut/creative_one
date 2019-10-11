a94027acaf1b8da17d2ca2b9d4b3427d9b891199
Hayden Reed <hreed787@gmail.com>
2019-10-11 19:20:17 +0000

    Added a button to test fetching api

--- spartan/js/script.js
+++ spartan/js/script.js
@@ -2,22 +2,22 @@
 //YOU DO NOT NEED A KEY TO ACCESS THE DAILY QUOTES! 
 //BUT IF YOU WANT TO DO OTHER STUFF THAT NEEDS A KEY, JUST SIGN UP!
 
-document.getElementById("getQuote").addEventListener("click", function(event) {
-  
+function myFunction() {
+        let results = "";
+
   //TO FETCH CURRENT WEATHER
   const url = "https://favqs.com/api/qotd";
   fetch(url)
     .then(function(response) {
       return response.json();
     }).then(function(json) {	
-      let results = "";
       results += '<h2>Weather in ' + json.quote + "</h2>";
      });
       
-      results += "</p><hr/ style='border: 2px solid white'>";
+      results += "<hr/ style='border: 2px solid white'>";
       document.getElementById("getQuote").innerHTML = results;
     
   
 
-});
+};
 
